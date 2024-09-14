7d4bde591021d7976a49bfc37500796a486a9ac9
RaulCasado <raulcasado969@gmail.com>
2024-09-14 13:27:22 +0200

    added strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racasado <racasado@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 13:17:36 by racasado          #+#    #+#             */
+/*   Updated: 2024/09/14 13:25:25 by racasado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	ft_strcpy(char *dest, const char *src, int *index)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[*index] = src[i];
+		(*index)++;
+		i++;
+	}
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	int		i;
+	int		s1_len;
+	int		s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	ptr = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	ft_strcpy(ptr, s1, &i);
+	ft_strcpy(ptr, s2, &i);
+	ptr[i] = '\0';
+	return (ptr);
+}
